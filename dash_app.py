4e404ab990fb9ec85af34cb5b3ba7d78104b9988
Sachin <sachingovender39@gmail.com>
2020-11-02 08:26:32 +0200

    Switched to single stock view

--- dash_app.py
+++ dash_app.py
@@ -9,11 +9,14 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
+from threading import Thread
+import pytz as tz
+from datetime import datetime
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 #Preloaded symbols
-symbols=['EURUSD','USDZAR']
+symbol=['EURUSD']
 #navbar
-
+##static components
 #input section 
 input_form=html.Div([dbc.Form(
     [
@@ -26,48 +29,55 @@ input_form=html.Div([dbc.Form(
         ),
         dbc.Button("Submit", color="dark",id="button",n_clicks=0),
     ],inline=True
-)])
-#graph
-graph_list=[]
-output_list=[]
-for i in symbols:
-    graph_id = i+'_graph'
-    graph_list.append(dcc.Graph(id=graph_id, animate=True, style={"backgroundColor": "#1a2d46", 'color':'#ffffff'}))
-    output_list.append(Output(graph_id,'figure'))
-
-graphs = html.Div(id='graphs',children=graph_list)
-
+)],style={'backgroundColor':"#1a2d46", 'color':'#ffffff','padding-bottom':'2%','padding-top':'2%'})
+#timer
 timer=dcc.Interval(
             id='interval-component',
             interval=60*1000, # in milliseconds
             n_intervals=0
         )
 
+#dynamic graphing function
+graph_list=[dcc.Graph(id='graph', animate=True, style={"backgroundColor": "#1a2d46", 'color':'#ffffff'})]
+output_list=[Output('graph','figure')]
+graphs = html.Div(id='graphs',children=graph_list,style={"backgroundColor": "#1a2d46", 'color':'#ffffff'})
 body = html.Div([
-    input_form,graphs,timer
-])
-
+input_form,graphs,timer,html.P(id='place',style={'display':'none'})])
 app.layout=body
 api_key = 'J2443HCF3V58552B'
 period = 60
 fx = TimeSeries(key=api_key, output_format='pandas')
 ti = TechIndicators(key=api_key, output_format='pandas')
+
 ##########event handling###########
 
-@app.callback(output_list,
-              [Input('interval-component', 'n_intervals')],
-              #[State('input-box', 'value')]
-              )
-def update_layout(n):
-    figure_items=[]
-    #Getting Dataframes Ready
-    for i in symbols:
-        from_currency = i.upper()[0:3]
-        to_currency = i.upper()[3:]
-        data_fx,meta_fx = fx.get_intraday(symbol=i, interval='1min',outputsize='full')
+def monitor():
+    @app.callback(output_list,
+                [Input('interval-component', 'n_intervals'),Input('button', 'n_clicks')],
+                #[State('input-box', 'value')]
+                )
+    def update_layout(n,click):
+        figure_items=[]
+        #Getting Dataframes Ready
+        print(symbol)
+        data_fx,meta_fx = fx.get_intraday(symbol=symbol[0], interval='1min',outputsize='full')
         data_fx.columns=['Open','High','Low','Close','Volume']
         print(data_fx.head())
-        data_ti, meta_data_ti = ti.get_sma(symbol=i.upper(), interval='1min', time_period=period, series_type='close')
+        data_ti, meta_data_ti = ti.get_sma(symbol=symbol[0].upper(), interval='1min', time_period=period, series_type='close')
+        #######Timezone adjust################
+        format = '%Y-%m-%d %H:%M:%S'
+        old_timezone = tz.timezone(meta_fx['6. Time Zone'])
+        new_timezone = tz.timezone("Africa/Johannesburg")
+        ##convert index to SAST
+        def getlocalindex(index):   
+            local_index=[]
+            for i in index:
+                actual_datetime=datetime.strptime(str(i), format)
+                local_timestamp = old_timezone.localize(actual_datetime).astimezone(new_timezone) 
+                local_index.append(local_timestamp.strftime(format))
+            return local_index
+        data_fx.index=getlocalindex(data_fx.index)
+        data_ti.index=getlocalindex(data_ti.index)
         data_fx = data_fx.iloc[::-1]
         BuySide = go.Candlestick(
             x=data_fx.index,
@@ -86,7 +96,7 @@ def update_layout(n):
         mode='lines',
         name='SMA'
         )
-        data = [BuySide]
+        data = [BuySide,SMA]
 
         layout = go.Layout(
             paper_bgcolor='#27293d',
@@ -98,15 +108,15 @@ def update_layout(n):
         )
         figure_items.append({'data': data, 'layout': layout})
 
-    return figure_items
+        return figure_items
+monitor_thread = Thread(target=monitor)
+monitor_thread.start()
+@app.callback(Output('place','children'),[Input('button', 'n_clicks')],
+              [State('input-box', 'value')])
+def update_symbols(n_clicks,new_symbol):
+    print('adding')
+    symbol[0]=new_symbol.upper()
+    return new_symbol.upper()
 
-# @app.callback([Input('button', 'n_clicks')],
-#               [State('input-box', 'value')])
-# def update_symbols(n_clicks,new_symbol):
-#     print('adding')
-    # new_graph_id = new_symbol.upper()+'_graph'
-    # graph_list.append(dcc.Graph(id=new_graph_id, animate=True, style={"backgroundColor": "#1a2d46", 'color':'#ffffff'}))
-    # output_list.append(Output(graph_id,'figure'))
-    # symbols.append(new_symbol.upper())
 if __name__ == '__main__':
-    app.run_server(port=8088)
+    app.run_server(port=8088) 
